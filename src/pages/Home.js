38dcf47561be91c885668b227cc1b45effc7672c
Alex Southgate <southgateJA@cardiff.ac.uk>
2025-08-31 18:15:54 +0100

    New font; remove cloud

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,26 +16,12 @@ for scientific organisations. To do this, I make use of software
 engineering principles, plenty of practice with algorithms,
 knowledge of cloud & HPC systems, and scientific expertise.`;
 
-  let speed_slow = 100;
-  let speed_fast = 50;
-
   return (
     <div className="container-fluid home-container">
-        <div className="row text-center">
-            <div className="col">
                 <div style={{ marginLeft: '5%', marginTop: '5%', marginRight: '5%' }}>
                     <h1 style={{ fontSize: '6em', textAlign: 'center'}}> ALEX J. SOUTHGATE, PhD</h1>
                     <h2 style={{ fontSize: '3em', textAlign: 'center'}}> Bioinformatician / Research Engineer </h2>
                 </div>
-                <div style={{ fontSize: '3em', textAlign: 'center'}}>
-                  <FloatEffect className="additional-class">
-                      <img src={rainCloud} alt="Example" className="img-fluid" />
-                  </FloatEffect>
-                </div>
-
-            </div>
-          <div/>
-        </div>
     </div>
   );
 };
