998ea442da72ab2d5495c69ef7c1870f1423bdc2
Alex Southgate <southgateJA@cardiff.ac.uk>
2025-12-27 19:18:04 +0000

    remove wave, just put a description.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './common.css';
 import { getEvent } from '../api/requests'
-import rainCloud from '../assets/raincloud2.png';
+import Aphids from '../assets/plant-aphids.png';
 import waveVideo from "../assets/crash.mp4";  
 import { FloatEffect } from '../Effects';
 import Publications from './Publications';
@@ -9,36 +9,11 @@ import Projects from './Blog';
 
 
 const Home = () => {
-
-  const aboutme = `
-Croeso :) I am a research software engineer from Wales, UK.
-My work is focused on solving software engineering problems
-for scientific organisations. To do this, I make use of software
-engineering principles, plenty of practice with algorithms,
-knowledge of cloud & HPC systems, and scientific expertise.`;
-
   return (
     <div className="container-fluid home-container">
         <div style={{ marginLeft: '5%', marginTop: '0%', marginRight: '5%' }}>
 
             <div style={{ position: 'relative', paddingTop: '0px', top: '-100px', height: '200px', overflow: 'visible' }}>
-              <video
-                src={waveVideo}
-                width="600"
-                autoPlay
-                loop
-                muted
-                playsInline
-                style={{
-                  position: 'absolute',
-                  top: '-10px',       // sticks up, but inside padding
-                  left: '50%',
-                  transform: 'translateX(-50%)',
-                  zIndex: 0,
-                  maxWidth: '600px',
-                  width: '100vw',
-                }}
-              />
 
               <div
                 style={{
@@ -50,10 +25,9 @@ knowledge of cloud & HPC systems, and scientific expertise.`;
                 }}
               >
                 <h1>ALEX J. SOUTHGATE, PhD</h1>
-                <p> Figure 1: SPH simulation with 1 million particles and 2 liquid phases. Implemented with Vulkan on a cheap GPU. </p>
+                <p> Croeso. I am an engineer and computational biologist from South Wales. Currently, I work on drug discovery systems in industry. It is probably fair to say that I have an unusually broad range of interests in science and tech... occasionally this has led me to work in other fields such as gravitational wave physics. In my spare time, I like to grow plants, and am prototyping embedded systems for automation and instrumentation (perhaps toward some useful end one day). I am a fan of difficult video games, textbook problems, and dogs, which are a useful distraction from _gestures_outside_. </p>
               </div>
             </div>
-
         </div>
     </div>
   );
